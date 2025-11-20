63839f10ac134c68f1c9d0831689a9598c245a38
Niko11o <hardniko1990@gmail.com>
2025-11-20 21:07:08 +0300

    Настроил маршрут из приложения main в video. Теперь при нажатии на кнопку "Видео" в шапке мейна он перекидывает куда надо.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls',namespace='main')),
-    path('videos', include('videos.urls'), ),
+    path('videos/', include('videos.urls'), ),
 ]
 
 if settings.DEBUG:
