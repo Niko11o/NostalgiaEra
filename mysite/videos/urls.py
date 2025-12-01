012c685f261e8d2ed182c95e46d60d52e7bcbe4d
Niko11o <hardniko1990@gmail.com>
2025-12-01 18:05:58 +0300

    Релейтед работает! Выглядит всё вырвиглазно, но главное что сайт не упал. Всё ещё нужна рука дизайнера.

--- mysite/videos/urls.py
+++ mysite/videos/urls.py
@@ -6,7 +6,11 @@ app_name = 'videos'
 urlpatterns = [
     path('', video_list, name='videos_index'),  # отображение основной страницы videos
 
+    path('<slug:year_slug>/', video_list,
+         name='product_list_by_year'),
+
     path('<int:id>/<slug:slug>/', video_detail,
          name='video_detail'),
+
 
 ]
--- mysite/videos/views.py
+++ mysite/videos/views.py
@@ -1,23 +1,48 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Video
+from .models import Video, YearFilter
+
 
 # Create your views here.
 
 
-def video_list(request):
+def video_list(request, year_slug=None):
     videos = Video.objects.all()
-    return render(request, "videos/index.html", {"videos": videos})
+
+    year = None
+    if year_slug:
+        year = get_object_or_404(YearFilter, slug=year_slug)
+        videos = videos.filter(year=year)
+
+
+    return render(request, "videos/index.html", {"videos": videos, 'year': year})
 
 
 
 def video_detail(request, id, slug):
     selected = get_object_or_404(Video, id=id, slug=slug)
-
-    return render(request, 'videos/selected_video.html', {'selected': selected})
-
-
+    related_videos = Video.objects.filter(year=selected.year).exclude(id=selected.id)[:4]
+    return render(request, 'videos/selected_video.html', {'selected': selected, 'related_videos': related_videos})
 
 
+"""
+def product_list(request, category_slug=None):
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+
+    category = None
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+
+    return render(request,
+                  'main/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products} )
+
+
+"""
 
 
 
