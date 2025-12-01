b3cde187d87d5b054c7ff5ca420482907c413858
Niko11o <hardniko1990@gmail.com>
2025-12-01 17:00:03 +0300

    Пора сделать фильтрацию по категории. Они у меня будут разделены по годам.

--- mysite/videos/models.py
+++ mysite/videos/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.urls import reverse
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Название')
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('main:product_list_by_category', args=[self.slug])
+
 
 class Video(models.Model):
     name = models.CharField(max_length=40, db_index=True)
