69b7c585b12ee25c33f45e4fa017dacec8e9ba50
Niko11o <hardniko1990@gmail.com>
2025-12-10 22:11:36 +0300

    ВИДЕО: зарегал модель, сделал админку. В след заход буду пилить вьюху и шаблон под это всё.

--- mysite/games/admin.py
+++ mysite/games/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Games, YearFilter
 
 # Register your models here.
+
+
+
+class GamesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Games, GamesAdmin)
+
+class YearFilterAdmin(admin.ModelAdmin):
+    list_display = ['year', 'slug']
+    prepopulated_fields = {'slug': ('year',)} # Слаг заполняется в соответствии с названием товара
+
+admin.site.register(YearFilter, YearFilterAdmin)
--- mysite/games/models.py
+++ mysite/games/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+
+class YearFilter(models.Model):
+    year = models.CharField(max_length=100, db_index=True, verbose_name='Год', null=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('year',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.year
+
+    def get_absolute_url(self):
+        return reverse('###:product_list_by_year', args=[self.slug])
+
+
+
+class Games(models.Model):
+    year = models.ForeignKey(YearFilter, db_index=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, db_index=True)
+    genre = models.CharField(max_length=50, db_index=True)
+    description = models.CharField()
+    date = models.DateTimeField()
+    slug = models.SlugField(max_length=100, unique=100)
+    torrent_link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Добавление игры'
+
+    def get_absolute_url(self):
+        return reverse('games:###', args=[self.id, self.slug])
